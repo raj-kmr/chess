20fbd345115442d6067bf30f03837c384b5da3d7
Raj <raj3373y@gmail.com>
2025-05-09 16:45:35 +0530

    Completed backend code

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server  = http.createServer(app);
 const io = socket(server);
 const chess = new Chess();
 let players = {};
-let currentPlayer = "W";
+let currentPlayer = "w";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -18,11 +18,48 @@ app.get("/", (req, res) => {
     res.render("index", { title: "Chess Game"} );
 })
 
-io.on("connection", function( uniquesocket) {
+io.on("connection", function(uniquesocket) {
     console.log("connected");
+    console.log(players);
+
+    if(!players.white) {
+        players.white = uniquesocket.id;
+        uniquesocket.emit("playerRole", "w");
+    } else if (!players.black) {
+        players.black = uniquesocket.id;
+        uniquesocket.emit("playerRole", "b");
+    } else {
+        uniquesocket.emit("spectatorRole");
+    }
+
+    uniquesocket.on("disconnect", function() {
+        if(uniquesocket.id === players.white) {
+            delete players.white;
+        } else if (uniquesocket.id === players.black) {
+            delete players.black;
+        }
+    })
+
+    uniquesocket.on("move", function(move) {
+        try {
+            if(chess.turn() === "w" && uniquesocket.id !== players.white) return;
+            if(chess.turn() === "b" && uniquesocket.id !== players.black) return;
+
+            const result = chess.move(move);
+            if(result) {
+                currentPlayer = chess.turn();
+                io.emit("move", move);
+                io.emit("boardState", chess.fen());
+            } else {
+                console.log("Invalid move:  ", move);
+                uniquesocket.emit("invalidMove", move);
+            }
+        } catch(err){
+            console.error(err);
+            uniquesocket.emit("Invalid move: ", move);
+        }
+    })
 })
 
-server.listen(3000, () => {
-    console.log("sever started on : 3000")
-})
+server.listen(3000)
 
